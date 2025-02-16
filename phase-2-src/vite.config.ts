e4d6c86c89c90b8bf9b534a6a70851be05fcd1d2
varma02 <varma02.marci@gmail.com>
2025-02-16 18:11:43 +0100

    [phase-2] add built project

--- phase-2-src/vite.config.ts
+++ phase-2-src/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   build: {
-    outDir: '../plase-2/',
+    outDir: '../phase-2/',
   },
   plugins: [react()],
 })
